6f89417fbca0e41d55aa226aad9dc91b264414c8
PiniR <rpini@cambium.co.il>
2015-03-25 17:48:05 +0200

    more ajax

--- wp-content/plugins/multipro-plugin/multi.php
+++ wp-content/plugins/multipro-plugin/multi.php
@@ -2,6 +2,7 @@
     class JSON_API_Multi_Controller{
     
         public function getYears(){
+           header("Access-Control-Allow-Origin: *");
            $yearsObj=get_terms( 'test-year');
             $years=array();
             foreach($yearsObj as $year){
@@ -10,6 +11,7 @@
             return  ( $years);
         }
         public function getTestNames(){
+            header("Access-Control-Allow-Origin: *");
             $testArray = get_posts( 'post_type=test' );
             $names=array();
             foreach($testArray as $test){
@@ -18,11 +20,13 @@
             return  ($names);
         }
         public function getTestNameForId(){
+            header("Access-Control-Allow-Origin: *");
             global $json_api;
             $id=$json_api->query->id;
             return  $test=get_post($id)->post_title;
         }
         public function getQuestionsForTest(){
+            header("Access-Control-Allow-Origin: *");
             global $json_api;
             $id=$json_api->query->id;
             $test=get_post_meta($id,'questions');
@@ -32,6 +36,17 @@
            }	
            return $questionArr;
         }
+        public function getTests(){
+            header("Access-Control-Allow-Origin: *");
+            global $json_api;
+            $testArray = get_posts( 'post_type=test' );
+            $testArr=array();
+            for($i=0;$i<sizeof($testArray);$i++){
+                $testArr[]=new testData($testArray[$i]->ID);
+            }	
+           return $testArr;       
+        }
+        
          
 		
 
@@ -43,14 +58,35 @@
         public $question; 
         public $answers;
         public $correctAns;
+        public $bookReferance;
+        public $year;
 
         function __construct($_id) {
             $this->id=$_id;
             $this->question=get_post_meta($_id,'wpcf-question');
             $this->answers=get_post_meta($_id,'wpcf-answer');
             $this->correctAns=get_post_meta($_id,'wpcf-correct');
+            $this->bookReferance=get_post_meta($_id,'wpcf-book-referance');
+            $temp=wp_get_post_terms( $_id, 'test-year');
+            $this->year=$temp[0]->name;
         }        
     }//class questionContect
+
+    class testData{
+        public $id;
+        public $numberOfQuestions;
+        public $title;
+
+         function __construct($_id) {
+                $this->id=$_id;
+                $temp=get_post_meta($_id,'questions');
+                $this->numberOfQuestions=sizeof($temp[0]);
+                $this->title=get_post($_id)->post_title;
+        }
+    }
+
+   
+    
 
 
 
